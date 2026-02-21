1a3efc3743294a7762b0d2f07729e9c7f2065ac4
Davide Murro <118051417+davide-murro@users.noreply.github.com>
2026-02-22 00:18:44 +0100

    backend: configure environment
    - add env example file
    - remove unused package
    - set cmd scripts for local running

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+
 const Task = require('./models/Task');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
+const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+
 
 // Routes
 app.use('/api/tasks', taskRoutes);
@@ -26,8 +25,8 @@ const startServer = async () => {
         await Task.sync();
         console.log('Database synchronized successfully.');
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        app.listen(PORT, HOST, () => {
+            console.log(`Server is running on http://${HOST}:${PORT}`);
         });
     } catch (error) {
         console.error('Unable to start the server:', error);
